b7b56d22410cd2c5ae37b1ea919b1d11d5c6b06d
Piyush-t24 <piyushiitian2023@gmail.com>
2025-01-13 23:18:42 +0530

    JS

---
+++ JS/Truthy and Falsy/app.js
@@ -0,0 +1,35 @@
+if (true){
+    console.log ("it has true value");
+}else{
+    console.log("it has false value");
+}
+
+
+if (false){
+    console.log ("it has true value");
+}else{
+    console.log("it has false value");
+}
+
+
+if (1){
+    console.log ("it has true value");
+}else{
+    console.log("it has false value");
+}
+
+
+if (0){
+    console.log ("it has true value");
+}else{
+    console.log("it has false value");
+}
+
+
+let string= "";
+
+if (string ){
+    console.log("string is not empty")
+}else {
+    console.log ("string is empty")
+}
