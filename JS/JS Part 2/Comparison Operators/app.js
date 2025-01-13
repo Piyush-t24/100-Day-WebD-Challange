c038203bfba7808c88ce0f8f00a50380199e12f3
Piyush-t24 <piyushiitian2023@gmail.com>
2025-01-14 00:13:24 +0530

    Comparison Operators

---
+++ JS/JS Part 2/Comparison Operators/app.js
@@ -0,0 +1,18 @@
+//Operators
+let age =18;
+console.log(age>18);  //false
+console.log(age<18);  //false
+
+
+console.log(age>16);  //true
+console.log(5==5 );  //true
+console.log(5==4 );  //false
+console.log(5!=5 );  //false
+console.log(5!=4 );  //true
+console.log(5=='5'); //true
+
+
+console.log(0==' ' );  //true
+console.log(0===' ');  //false
+
+
---
+++ JS/JS Part 2/Comparison for Non- number/app.js
@@ -0,0 +1,4 @@
+console.log('a'< 'A');  //false
+console.log('a'< 'b');  //true
+console.log('*'< '&');  //false
+console.log('*'> '&');  //true
