d6577e2fd47a9e1843d4e2a457f252159bf65511
Piyush-t24 <piyushiitian2023@gmail.com>
2025-01-13 23:51:51 +0530

    Operators in JS

---
+++ JS/Operators in JS/app.js
@@ -0,0 +1,15 @@
+//Arithmetic Operators
+let a =10;
+let b=5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+//Unary
+console.log(a++);  //10
+console.log(++a);  //12
+//Assignment
+b=a;
+console.log(b);  //12
