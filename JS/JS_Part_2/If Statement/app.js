56b3f3a74870a733c18ed7a3e0f9da9260e28a61
Piyush-t24 <piyushiitian2023@gmail.com>
2025-01-14 15:57:21 +0530

    ADD

---
+++ JS/JS_Part_2/If Statement/app.js
@@ -0,0 +1,16 @@
+//Conditional Statements
+console.log("before my if statement");
+let age = 23;
+if(age>= 18){
+    console.log("you can vote");
+    console.log("you can drive");
+}
+if(age< 18){
+    console.log("you cannot vote");
+    console.log("you cannot drive");
+}
+console.log("after my if statement");
+let firstName = "Piyush";
+if(firstName=="Piyush"){
+    console.log(`Welcome ${firstName}`)
+} 
