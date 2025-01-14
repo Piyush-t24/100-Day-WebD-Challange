ded5b97cc353980a108f8259340f0fd5192315c1
Piyush-t24 <piyushiitian2023@gmail.com>
2025-01-14 18:40:18 +0530

    String Methods

---
+++ JS/JS_Part_3/String Methods/Trim Method/app.js
@@ -0,0 +1,11 @@
+// let msg = "   hello    ";
+
+
+let password = prompt("set your passwprd");
+let newPass = password.trim();
+console.log(newPass);
+
+//String are Immutable in JS
+// No changes can be made to strings.
+// Whenever we do try to make a change , a new string is created and old one remains same
+
