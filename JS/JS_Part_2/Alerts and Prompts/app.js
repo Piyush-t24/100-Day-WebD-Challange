63cc156e18ec753ee713a0127990be162924620a
Piyush-t24 <piyushiitian2023@gmail.com>
2025-01-14 16:22:12 +0530

    Practice Questions

---
+++ JS/JS_Part_2/Alerts and Prompts/app.js
@@ -0,0 +1,19 @@
+//alert("Something is wrong!")
+
+console.log ("this is a simple log");
+
+console.error ("this is an error msg");
+
+console.warn ("this is a warning msg");
+
+
+
+//prompt("Enter your name: ");
+
+// let firstName = prompt("Enter your name: ");
+// console.log(firstName);
+
+
+let firstName = prompt("Enter first name");
+let lastName = prompt("Enter last name");
+console.log("Welcome", firstName, " " , lastName, "!");
