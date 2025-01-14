5bb4e99f08be22fc84464d0099ac578aa8cee08e
Piyush-t24 <piyushiitian2023@gmail.com>
2025-01-14 22:15:04 +0530

    Array

---
+++ JS/JS_Part_3/Array/app.js
@@ -0,0 +1,20 @@
+// let student1 = "aman";
+// let student2 = "piyush";
+// let student1 = "ankit";
+
+let students = ["aman","piyush","ankit  "];
+
+let nums = [2,4,6,8];
+
+//Creating Arrays
+
+let marks = [99,85,93,76,62];
+let names = ["adam", "bob", "catlyn"];
+let info = ["aman", 25, 6.1]; //mixed array
+//empty array
+let newArr = [];
+
+//Arrays are Mutable
+let fruits = ["mango", "apple", "litchi"];
+fruits[0]= "banana";
+console.log(fruits);
