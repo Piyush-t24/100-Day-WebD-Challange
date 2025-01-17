7dc0ee9656b13e05c148d63a7622f064e61caa99
Piyush-t24 <piyushiitian2023@gmail.com>
2025-01-17 20:29:49 +0530

    Function

---
+++ JS/JS_Part_6/Functions/app.js
@@ -0,0 +1,26 @@
+function hello(){
+    console.log("hello");
+}
+hello();
+
+function printName(){
+    console.log("apnacollege");
+}
+printName();
+
+function print1to5(){
+    for(let i=1;i<=5;i++){
+        console.log(i);
+    }
+}
+print1to5();
+
+function isAdult(){
+    let age = 17;
+    if(age >= 18){
+        console.log("adult");
+    }else{
+        console.log("not adult");
+    }
+}
+isAdult();
