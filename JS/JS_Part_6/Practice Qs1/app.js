54cf9b1e766e6fc615723ba7fbbf8417d3e0f702
Piyush-t24 <piyushiitian2023@gmail.com>
2025-01-18 22:39:46 +0530

    Practice Qs

---
+++ JS/JS_Part_6/Practice Qs1/app.js
@@ -0,0 +1,7 @@
+// Qs. Create a function that print a poem.
+
+function printPoem (){
+    console.log("Twinkle Twinkle ,little star");
+    console.log("How I wonder what you are");
+}
+printPoem();
