eee31b7c77b10d089e576bb6485646c872dd8271
Piyush-t24 <piyushiitian2023@gmail.com>
2025-01-19 01:05:07 +0530

    Function

---
+++ JS/JS_Part_6/Function with Arguments/app.js
@@ -0,0 +1,16 @@
+function printName(name){
+    console.log(name);
+}
+
+printName("piyush");
+
+function printInfo(name , age){
+    console.log(`${name}'s age is ${age}.`);
+}
+
+printInfo("piyush", "19");
+
+function sum (a, b){
+    console.log(a+b);
+}
+sum(23,567);
