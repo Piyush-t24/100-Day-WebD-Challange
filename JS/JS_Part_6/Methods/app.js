2ba5f35d4d55ff2efe30da92bbd0893ba31524c4
Piyush-t24 <piyushiitian2023@gmail.com>
2025-01-19 18:14:12 +0530

    Methods

---
+++ JS/JS_Part_6/Methods/app.js
@@ -0,0 +1,16 @@
+const calculator = {
+    num: 55,
+    add: function(a,b){
+        return a+b;
+    },
+    sub: function(a,b){
+        return a-b;
+    },
+    mul: function(a,b){
+        return a*b;
+    }
+}
+console.log(calculator.num);
+console.log(calculator.add(1,2));
+console.log(calculator.sub(54,34));
+console.log(calculator.mul(2,5));
