def985dceded5a53b552d11357b49a001c35047a
Piyush-t24 <piyushiitian2023@gmail.com>
2025-01-20 02:01:18 +0530

    This

---
+++ JS/JS_Part_7/Arrow Functions/app.js
@@ -0,0 +1,16 @@
+const sum = (a, b) => {
+    console.log(a+b);
+};
+console.log(sum(2,3));
+
+
+const cube = (n) => {
+    return n*n*n;
+};
+console.log(cube(3));
+
+
+const pow = (a,b) => {
+    return a**b;
+};
+console.log(pow(2,3));
---
+++ JS/JS_Part_7/Try & Catch/app.js
@@ -0,0 +1,26 @@
+console.log("hello");
+console.log("hello");
+try{
+    console.log(a);
+}catch{
+    console.log("cought an error.. a is not defined");
+}
+
+console.log("hello2");
+console.log("hello2");
+console.log("hello2");
+
+
+console.log("hello");
+console.log("hello");
+let a = 5;
+try {
+  console.log(a);
+} catch(err) {
+  console.log("cought an error.. a is not defined");
+  console.log(err);
+}
+
+console.log("hello2");
+console.log("hello2");
+console.log("hello2");
