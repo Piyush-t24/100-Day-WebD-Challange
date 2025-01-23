8ccd256fc86b094295934071ddeadc1c9d0e5656
Piyush-t24 <piyushiitian2023@gmail.com>
2025-01-23 11:31:19 +0530

    nodejs

--- Backend/nodejs/script2.js
+++ Backend/nodejs/script2.js
@@ -1,3 +1,7 @@
-const math = require("./math");
+// const math = require("./math");
 
-console.log(math);
+// console.log(math.sum(2,2));
+// console.log(math.PI);
+
+const info = require("../Fruits");
+console.log(info);
