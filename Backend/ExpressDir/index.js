2d18fcd12e1b8e5f7321fcde184946246ae1f315
Piyush-t24 <piyushiitian2023@gmail.com>
2025-01-25 00:58:50 +0530

    Parsing

--- Backend/ExpressDir/index.js
+++ Backend/ExpressDir/index.js
@@ -8,5 +8,8 @@ app.listen(port, () => {
 });
 
 app.use((req, res) => {
+    // console.log(req);
     console.log("request received");
+    let code = "<h1> Fruits </h1> <ul><li>apple</li><li>orange</li></ul>";
+    res.send(code);
 });
