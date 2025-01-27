a2c4c0e5267ba58c14cd4fe13b9b63af916a38e0
Piyush-t24 <piyushiitian2023@gmail.com>
2025-01-27 23:48:32 +0530

    Spread

---
+++ JS/JS_Part_8/Spread(ArrayLiterals)/app.js
@@ -0,0 +1,18 @@
+let arr = [1,2,3,4,5];
+let newArr = [...arr];
+console.log(arr);
+console.log(newArr);
+newArr.push(6);
+console.log(newArr);
+
+
+
+
+let chars = [..."hello"];
+console.log(chars);
+
+let odd = [1,3,5,7,9];
+let even = [2,4,6,8,10];
+let nums = [...odd, ...even];
+console.log(nums);
+console.log(...nums);
