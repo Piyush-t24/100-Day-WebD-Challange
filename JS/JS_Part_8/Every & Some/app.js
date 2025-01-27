7df5c7826dfbf121f326b9f4c7ce47e7d826b67c
Piyush-t24 <piyushiitian2023@gmail.com>
2025-01-27 15:16:23 +0530

    Every and Some

---
+++ JS/JS_Part_8/Every & Some/app.js
@@ -0,0 +1,15 @@
+// Every
+
+let num = [2,4,6,5];
+
+let even=num.every((el) => (el %2 == 0));
+
+console.log(even);
+
+// Some
+
+let arr = [2, 4, 6, 5, 7,9,3];
+
+let some = arr.some((el) => el % 2 == 0);
+
+console.log(some);
