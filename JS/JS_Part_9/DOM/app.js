3fbe4ecb11a05d391aa6a7c717ce25cc188332c9
Piyush-t24 <piyushiitian2023@gmail.com>
2025-01-29 20:53:33 +0530

    Query Selector

--- JS/JS_Part_9/DOM/app.js
+++ JS/JS_Part_9/DOM/app.js
@@ -5,3 +5,13 @@ for (let i = 0; i < smallImages.length; i++) {
   console.dir(smallImages[i].src);
 }
 
+// Query Selector
+console.dir(document.querySelector("h1"));
+console.dir(document.querySelector("#description"));
+console.dir(document.querySelector(".oldImg"));
+console.dir(document.querySelector("div a"));       //first ancer tag aa jayega
+
+
+console.dir(document.querySelectorAll("p"));
+console.dir(document.querySelectorAll("div a"));
+
