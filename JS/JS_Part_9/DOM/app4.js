32fbc0a7080364399964ca3cbb0260fa85c8054f
Piyush-t24 <piyushiitian2023@gmail.com>
2025-01-30 22:51:57 +0530

    ADD

---
+++ JS/JS_Part_9/DOM/app4.js
@@ -0,0 +1,16 @@
+// Navigation
+
+let h4 = document.querySelector("h4");
+
+console.log(h4.parentElement);
+
+console.log(h4.children);
+
+let box = document.querySelector(".box");
+
+console.log(box.children);
+console.log(box.childElementCount);
+
+let ul = document.querySelector("ul");
+console.log(ul.parentElement);
+console.log(ul.childElementCount);
