dd6c26b07d61e80da7aa66ad61c5d56605e03a96
Piyush-t24 <piyushiitian2023@gmail.com>
2025-02-04 23:56:28 +0530

    Handling rejection

--- JS/JS_Part_12/Async and Await/app.js
+++ JS/JS_Part_12/Async and Await/app.js
@@ -1,55 +1,70 @@
-async function greet() {
-  // throw "404 page not found";
-  return "hello!"; //return a promise
-}
+// async function greet() {
+//   // throw "404 page not found";
+//   return "hello!"; //return a promise
+// }
 
-greet()
-  .then((result) => {
-    console.log("promise was resolved");
-    console.log("result was : ", result);
-  })
-  .catch((err) => {
-    console.log("promise was rejected with err: ", err);
-  });
+// greet()
+//   .then((result) => {
+//     console.log("promise was resolved");
+//     console.log("result was : ", result);
+//   })
+//   .catch((err) => {
+//     console.log("promise was rejected with err: ", err);
+//   });
 
-  let demo = async()=> {
-    return 5;
-  } ;
+// let demo = async () => {
+//   return 5;
+// };
 
-// Await Keyword
+// // Await Keyword
 
-function getNum(){
-  return new Promise((resolve, reject)=> {
-    setTimeout(()=> {
-      let num = Math.floor(Math.random()*10)+1;
-      console.log(num);
-      resolve();
-    }, 1000);
-  });
-}
-async function demo() {
-  await getNum();
-  await getNum();
-  await getNum();
-}
+// function getNum() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       let num = Math.floor(Math.random() * 10) + 1;
+//       console.log(num);
+//       resolve();
+//     }, 1000);
+//   });
+// }
+// async function demo() {
+//   await getNum();
+//   await getNum();
+//   await getNum();
+// }
+
+// Color wala code with handling of rejections
 
 h1 = document.querySelector("h1");
 
 function changeColor(color, delay) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
+      let num = Math.floor(Math.random()*5)+1;
+      if(num >3){
+        reject("promise rejected");
+      }
+
+
       h1.style.color = color;
+      console.log(`color changed to ${color}`);
       resolve("color changed!");
     }, delay);
   });
 }
 
 async function demo() {
-  await changeColor("violet", 1000);
-  await changeColor("indigo", 1000);
-  await changeColor("green", 1000);
-  await changeColor("yellow", 1000);
-  await changeColor("orange", 1000);
-
-  return "done";
+  try {
+    await changeColor("violet", 1000);
+    await changeColor("indigo", 1000);
+    await changeColor("green", 1000);
+    await changeColor("yellow", 1000);
+    await changeColor("orange", 1000);
+  } catch (err) {
+    console.log("error cought");
+    console.log(err);
+  }
+  let a = 5;
+  console.log(a);
+  console.log("new number =", a +3);
 }
