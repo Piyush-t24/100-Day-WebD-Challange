546bc77df0588835c212895a6e806788ba5e274b
Piyush-t24 <piyushiitian2023@gmail.com>
2025-02-06 00:54:22 +0530

    add

---
+++ JS/JS_Part_13/Axios/app.js
@@ -0,0 +1,22 @@
+let btn = document.querySelector("button");
+btn.addEventListener("click", async()=>{
+    getFacts();
+    let fact= await getFacts();
+    console.log(fact);
+
+    let p = document.querySelector("#result");
+    p.innerText = fact;
+
+})
+
+let url = "https://catfact.ninja/fact";
+
+async function getFacts() {
+  try {
+    let res = await axios.get(url);
+    return res.data.fact;
+  } catch (e) {
+    console.log("ERROR -", e);
+    return "No fact found";
+  }
+}
