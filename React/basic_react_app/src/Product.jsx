71443216de3c44108916e0e16333c9e34e3e2940
Piyush-t24 <piyushiitian2023@gmail.com>
2025-02-09 13:17:31 +0530

    add

--- React/basic_react_app/src/Product.jsx
+++ React/basic_react_app/src/Product.jsx
@@ -1,10 +1,11 @@
-import "./Product.css"
+import "./Product.css";
 
-function Product({title,price}) {
+function Product({ title, price, features }) {
   return (
     <div className="Product">
       <h3>{title}</h3>
       <h5>Price: {price}</h5>
+      {price > 30000 && <p>Discount of 5%</p>}
     </div>
   );
 }
--- React/basic_react_app/src/ProductTab.jsx
+++ React/basic_react_app/src/ProductTab.jsx
@@ -1,11 +1,12 @@
 import Product from "./Product.jsx";
 
 function ProductTab() {
+  let options = ["hi-tech", "durable", "fast"];
   return (
     <>
-      <Product title="Phone"  price={30000}/>
+      <Product title="Phone"  price={30000} />
       <Product title="Laptop" price={40000}/>
-      <Product title="Pen"  price={10}/>
+      <Product title="Pen"  price={1}/>
     </>
   );
 }
