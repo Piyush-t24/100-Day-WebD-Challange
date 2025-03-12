ffb05aede48444a9eedb9eff779fce034132c52c
Piyush-t24 <piyushiitian2023@gmail.com>
2025-03-13 01:44:55 +0530

    Amazon Cards

--- React/Activity/src/ProductTab.jsx
+++ React/Activity/src/ProductTab.jsx
@@ -1,13 +1,20 @@
 import Product from "./product.jsx";
 
 function ProductTab() {
+  let styles = {
+      display : "flex",
+      flexWrap : "wrap",
+      justifyContent: "center",
+      alignItems : "center",
+  };
+
   return (
-    <>
+    <div style={styles}>
       <Product  title="Logitech MX Master"  idx={0}/>
       <Product  title="Apple Pencil (2nd Gen)" idx={1}/>
       <Product  title="Zebronics" idx={2}/>
       <Product  title="Petronics Toad" idx={3}/>
-    </>
+    </div>
   );
 }
 
