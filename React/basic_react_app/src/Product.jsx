a6d722309a3de8501794ab196d758861d95cb927
Piyush-t24 <piyushiitian2023@gmail.com>
2025-03-12 16:45:42 +0530

    add

--- React/basic_react_app/src/Product.jsx
+++ React/basic_react_app/src/Product.jsx
@@ -1,13 +1,13 @@
 import "./Product.css";
 
 function Product({ title, price, features }) {
-  let isDiscount = price>30000;
-  let styles = {backgroundColor : isDiscount ?  "lightblue" : ""};
+  let isDiscount = price > 30000;
+  let styles = {backgroundColor : isDiscount ? "lightblue" : ""};
   return (
     <div className="Product" style={styles}>
       <h3>{title}</h3>
       <h5>Price: {price}</h5>
-      {isDiscount && <p>Discount of 5%</p>}
+      {isDiscount ? <p>Discount of 5%</p> : null}
     </div>
   );
 }
