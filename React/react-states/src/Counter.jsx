35084f8fe042b0ce04e51c2a0d60ae6aa19842cf
Piyush-t24 <piyushiitian2023@gmail.com>
2025-03-14 01:24:30 +0530

    States

--- React/react-states/src/Counter.jsx
+++ React/react-states/src/Counter.jsx
@@ -3,12 +3,24 @@ import { useState } from "react";
 
 export default function Counter(){
     // let [stateVariable, setStateVariable] = useState(0);
-    let [count , setCount] = useState(0);
+    let [count , setCount] = useState(0);         //initialization  (ignored in re-render)
 
     let incCount = ()=> {
-        setCount(count+1);
-        
+        setCount(count+2);   
     };
+
+
+    // OR
+    
+    // let incCount = () => {
+    //     setCount((currCount)=> {
+    //         return currCount + 1;
+    //     });
+    //     setCount((currCount)=> {
+    //         return currCount + 1;
+    //     });
+    // }
+
 
    return(
     <div>
