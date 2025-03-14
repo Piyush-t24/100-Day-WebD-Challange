20d059eb9a1e37f32ac3f57e4e398fb093a2de02
Piyush-t24 <piyushiitian2023@gmail.com>
2025-03-14 23:55:24 +0530

    LudoBoard

--- React/react-states-2/src/App.jsx
+++ React/react-states-2/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import LudeBoard from './LudoBoard'
 import './App.css'
 
 function App() {
-  
+  return(
+    <>
+    <LudeBoard/>
+    </>
+  )
 }
 
 export default App
