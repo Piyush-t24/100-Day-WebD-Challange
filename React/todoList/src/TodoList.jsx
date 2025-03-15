3dd1a56f6ab985def8c49203b5515a212ab46d80
Piyush-t24 <piyushiitian2023@gmail.com>
2025-03-15 13:58:37 +0530

    unique id

--- React/todoList/src/TodoList.jsx
+++ React/todoList/src/TodoList.jsx
@@ -1,33 +1,37 @@
-import { use, useState } from "react"
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-export default function TodoList(){
-   let [todos, setTodos] = useState (["Sample Task"]);
-   let [newTodo, setNewTodo] = useState ("");
-   
-   let addNewTask = ()=>{
-        setTodos([...todos, newTodo]);
-        setNewTodo("");
-   }
-   let updateTodoValue = (event)=>{
+export default function TodoList() {
+  let [todos, setTodos] = useState([{ task: "sample-task", id: uuidv4() }]);
+  let [newTodo, setNewTodo] = useState("");
+
+  let addNewTask = () => {
+    setTodos([...todos, {task: newTodo , id: uuidv4()}]);
+    setNewTodo("");
+  };
+  let updateTodoValue = (event) => {
     setNewTodo(event.target.value);
-   }
+  };
 
-   return(
+  return (
     <div>
-        <input placeholder="add a task" value={newTodo} onChange={updateTodoValue}></input>
-        <br />
-        <button onClick={addNewTask}>Add Task</button>
-        <br /><br /><br />
-        <hr />
-        <h4>Tasks Todo</h4>
-        <ul>
-            {
-                todos.map((todo)=>(
-                    <li>{todo}</li>
-                ))
-            }
-        </ul>
-
+      <input
+        placeholder="add a task"
+        value={newTodo}
+        onChange={updateTodoValue}
+      ></input>
+      <br />
+      <button onClick={addNewTask}>Add Task</button>
+      <br />
+      <br />
+      <br />
+      <hr />
+      <h4>Tasks Todo</h4>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.task}</li>
+        ))}
+      </ul>
     </div>
-   ) 
+  );
 }
