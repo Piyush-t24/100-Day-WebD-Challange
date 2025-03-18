7f8c759a63ec87d0b43d01f546d474fd9165bf2e
Piyush-t24 <piyushiitian2023@gmail.com>
2025-03-18 15:46:23 +0530

    add

--- React/todoList/src/App.jsx
+++ React/todoList/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import TodoList from './TodoList'
 import Lottery from './Lottery'
+import Ticket from './Ticket'
 
 function App() {
   return(
     <>
-    <Lottery/>
+    <Ticket ticket={[0, 1, 2]} />
     </>
   )
 }
