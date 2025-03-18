c6377c1a4079900fedc07623044678e1cce785bd
Piyush-t24 <piyushiitian2023@gmail.com>
2025-03-19 00:02:41 +0530

    add

--- React/todoList/src/App.jsx
+++ React/todoList/src/App.jsx
@@ -3,11 +3,20 @@ import TodoList from './TodoList'
 import Lottery from './Lottery'
 import Ticket from './Ticket'
 import Lottery2  from './Lottery2'
+import { sum } from './helper'
 
 function App() {
+  let winCondition = (ticket) =>{
+    // return sum(ticket) === 15;
+    // return ticket[0] === 0;
+    return ticket.every((num)=> num === ticket[0]);
+  }
+
   return(
     <>
-      <Lottery2 n={3} winningSum={15}/>
+      {/* <Lottery2 n={3} winningSum={15} /> */}
+      <Lottery2 n={3} winCondition={winCondition} /> 
+       {/* we pass whole function in props */}
     </>
   )
 }
--- React/todoList/src/Lottery2.jsx
+++ React/todoList/src/Lottery2.jsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 import { genTicket, sum } from "./helper";
 import Ticket from "./Ticket";
 
-export default function Lottery({n =3, winningSum =15}) {
+export default function Lottery({n =3, winCondition =15}) {
   let [ticket, setTicket] = useState(genTicket(n));
-  let isWinning = sum(ticket) === winningSum;
+  // let isWinning = sum(ticket) === winningSum;
+  let isWinning = winCondition(ticket);
   let buyTicket = () => {
     setTicket(genTicket(n));
   };
