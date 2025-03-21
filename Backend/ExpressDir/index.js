6f33c65222499e3a07ae92f1ca78f36fc250d8a5
Piyush-t24 <piyushiitian2023@gmail.com>
2025-03-22 01:13:29 +0530

    add

--- Backend/ExpressDir/index.js
+++ Backend/ExpressDir/index.js
@@ -4,13 +4,41 @@ const app = express();
 let port = 3000;
 
 app.listen(port, () => {
-    console.log(`app is listening on port ${port}`);
+  console.log(`app is listening on port ${port}`);
 });
 
-app.use((req, res) => {
-    // console.log(req);
-    console.log("request received");
-    let code = "<h1> Fruits </h1> <ul><li>apple</li><li>orange</li></ul>";
-    res.send(code);
-    
+app.get("/", (req, res) => {
+  res.send("hello , i am root");
 });
+
+app.get("/:username/:id", (req, res) => {
+  let {username , id}= req.params;
+  res.send(`welcometo the page of @${username}`);
+});
+
+app.get("/search", (req, res)=>{
+    // console.log(req.query);
+    // res.send("no results");
+    let {q}= req.query;
+    if (!q){
+        res.send("No search query");
+    }
+    res.send(`search result for query: ${q}`);
+})
+
+
+// app.get("/apple", (req, res) => {
+//   res.send("you contacted apple path");
+// });
+// app.get("/orange", (req, res) => {
+//   res.send("you contacted orange path");
+// });
+// app.get("*", (req, res) => {
+//   res.send("this path does not exist");
+// });
+
+// app.use((req, res) => {
+//     console.log("request received");
+//     let code = "<h1> Fruits </h1> <ul><li>apple</li><li>orange</li></ul>";
+//     res.send(code);
+// });
