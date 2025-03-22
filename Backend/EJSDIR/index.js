31272ab2beba8dba76367c9d5e4169dcbd072dd7
Piyush-t24 <piyushiitian2023@gmail.com>
2025-03-23 01:51:11 +0530

    EJSDIR

--- Backend/EJSDIR/index.js
+++ Backend/EJSDIR/index.js
@@ -1,9 +1,15 @@
 const express = require ("express");
 const app = express();
+const path = require("path");
 
 const port = 8080;
 
 app.set("view engin", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res)=>{
+    res.render("home.ejs");
+})
 
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);
