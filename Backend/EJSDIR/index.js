dba72df0455ef7eff8d1ce7dc4605b1294e29bb3
Piyush-t24 <piyushiitian2023@gmail.com>
2025-03-23 22:52:12 +0530

    instagram page

--- Backend/EJSDIR/index.js
+++ Backend/EJSDIR/index.js
@@ -1,4 +1,4 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
 const path = require("path");
 
@@ -7,22 +7,32 @@ const port = 8080;
 app.set("view engin", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.get("/", (req, res)=>{
-    res.render("home.ejs");
+app.get("/", (req, res) => {
+  res.render("home.ejs");
 });
-app.get("/ig/:username", (req, res)=>{
-   let {username}= req.params;
-   res.render("instagram.ejs", {username});
+app.get("/ig/:username", (req, res) => {
+  // const followers = ["adam", "bob", "steve", "abc"];
+  // let { username } = req.params;
+  // res.render("instagram.ejs", { username, followers });
+
+  let { username } = req.params;
+  const instaData = require("./data.json");
+  const data = instaData[username];
+  if (data) {
+    res.render("instagram.ejs", { data });
+  }else{
+    res.render("error.ejs");
+  }
 });
 
-app.get("/hello", (req, res)=>{
-    res.render("home.ejs");
+app.get("/hello", (req, res) => {
+  res.render("home.ejs");
 });
-app.get("/rolldice", (req, res)=>{
-    let diceVal = Math.floor(Math.random() * 6) + 1;
-    res.render("rollDice.ejs",{diceVal});
+app.get("/rolldice", (req, res) => {
+  let diceVal = Math.floor(Math.random() * 6) + 1;
+  res.render("rollDice.ejs", { diceVal });
 });
 
-app.listen(port,()=>{
-    console.log(`listening on port ${port}`);
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
