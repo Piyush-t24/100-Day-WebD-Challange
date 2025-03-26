bed27dc60d6ca3c17466f05f290198febdc38611
Piyush-t24 <piyushiitian2023@gmail.com>
2025-03-27 01:16:25 +0530

    add

--- MISCELLANEOUS/Frontend/app2.js
+++ MISCELLANEOUS/Frontend/app2.js
@@ -1,6 +1,12 @@
 // OOPs
 
-let arr = [1, 2, 3];
-arr.sayHello = () => {
+let arr1 = [1, 2, 3];
+let arr2 = [1, 2, 3];
+
+arr1.sayHello = () => {
+    console.log("hello!, i am arr");
+};
+
+arr2.sayHello = () => {
     console.log("hello!, i am arr");
 };
