441ab01d84894b416a01b2a0c85e0ba51b09d339
Piyush-t24 <piyushiitian2023@gmail.com>
2025-03-28 23:48:21 +0530

    add

---
+++ TypeScript/types/any_unknown.ts
@@ -0,0 +1,51 @@
+//* ___________________________________________
+//* Any Type
+//* ___________________________________________
+// ? The any type is the most flexible type in TypeScript. It essentially turns off all type checking for the variables or expressions it is applied to.
+// ? you will see any type when u first write the code
+
+let myfavNum: any = 5;
+myfavNum = "vinod";
+myfavNum = true;
+
+// ! useCases
+
+// * Working with Dynamic Data: When dealing with data from dynamic sources like user inputs, network responses, or deserialized JSON objects, the any type can be useful.
+
+// * Migration from JavaScript: When migrating an existing JavaScript codebase to TypeScript, using the any can be a convenient way to quickly annotate variables and functions without immediately specifying their precise type.
+
+//* ___________________________________________
+//* Unknown Type
+//* ___________________________________________
+
+// ? The unknown type is a safer alternative to any because it still enforces type checking and type safety.
+// ? Variables of type unknown can hold values of any type, but you must perform type checks or type assertions before using them in specific ways.
+
+
+let num2: unknown;
+num2 = 5;
+num2 = "piyush";
+num2 = true;
+
+
+if(typeof num2 === "number"){
+    console.log(num2 + 5);
+}else if (typeof num2 === "boolean"){
+    console.log(num2);
+}
+
+
+// * Other Example of unknown
+
+// async function fetchData(): Promise<unknown>{
+//     const response = await fetch('https://api.example.com/data');
+//     const data = await response.json();
+//     return data;
+// }
+
+// async function processData() {
+//     const response = await fetchData();
+//     if(typeof response === 'object'){
+//         // perform operations on the response object
+//     }
+// }
