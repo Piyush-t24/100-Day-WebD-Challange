29e79a8edb66a31f703bcffbfb7e5704ba9a9bdb
Piyush-t24 <piyushiitian2023@gmail.com>
2025-03-31 12:18:47 +0530

    objects

---
+++ TypeScript/Remaining/object.ts
@@ -0,0 +1,28 @@
+//* In TypeScript , objects are used to represent data with key-value pairs.Each key in the object is a string (or a symbol in ES6) that maps to a value.
+
+// ? Let's consider a real life example of representing a person's information using Typescript objects
+// ? ->  name, age, isStudent, address{city, country}
+
+const person:{
+    name:string;
+    age:number;
+    isStudent:boolean;
+    address:{city:string; country:string}
+} = {
+  name: "Piyush Gupta",
+  age: 20,
+  isStudent: true,
+  address: {
+    city: "Pune",
+    country: "India",
+  },
+};
+
+// Todo Accessing the object data
+
+console.log(person.address.country);
+
+// Todo Updating object properties
+person.address.country= "Nepal";
+console.log(person.address.country);
+
