a94787bc55d49cf5daaf73035e1a465331452c92
Piyush-t24 <piyushiitian2023@gmail.com>
2025-04-04 01:34:13 +0530

    TS

--- TypeScript/Remaining/callSignatures.js
+++ TypeScript/Remaining/callSignatures.js
@@ -1,4 +1,5 @@
 //* The function call signature refers to the declaration or drfinition of a function, which includes the function's name, parameters, and return type. It defines the structure and type information of a function without including the function's implementation or body.
+
 var student1 = {
     name: "Piyush",
     age: 20,
@@ -6,6 +7,7 @@ var student1 = {
         return "Welcome My name is ".concat(student1.name, " , I am ").concat(student1.age, "yrs old & I am from ").concat(country);
     },
 };
+
 var student2 = {
     name: "Karan",
     age: 20,
@@ -17,6 +19,7 @@ var intro = function (student1) {
     var name = student1.name, age = student1.age;
     return "Welcome My name is ".concat(name, " , I am ").concat(age, "yrs old");
 };
+
 console.log(intro(student1));
 console.log(student1.greet("India"));
 console.log(student2.greet("Nepal"));
